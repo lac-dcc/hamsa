c8bd180935a27dc72d249e5a887144042bdbf71e
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2022-11-08 11:21:26 -0300

    Editing sample9.cpp

--- test/samples/sample9.cpp
+++ test/samples/sample9.cpp
@@ -1,11 +1,16 @@
-int main() {
-    int n = 5;
-    int m = 10;
-    for(int i = 0; i < m; i++) {
+void loop(int m, int n) {
+    for(int i = n; i < m; i++) {
         int a = 1 + 2;
-        for(int j = n; j >= 0; j--) {
+        for(int j = n * m; j >= 0; j--) {
             int b = 1 + a;
         }   
     }
+}
+
+int main() {
+    int n = 5 + 2;
+    int m = 10;
+    loop(m, n);
+
     return 0;
 }
