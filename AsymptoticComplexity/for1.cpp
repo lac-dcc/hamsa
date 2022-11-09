302f0e0772d3a6895f2798379199a2ad206ed0e0
1LucasVictor <1victor.lucassilva@gmail.com>
2022-11-09 08:31:41 -0300

    Adding a new case

--- AsymptoticComplexity/for1.cpp
+++ AsymptoticComplexity/for1.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 void MultMatriz(int** m1, int** m2, int m, int n, int q, int** m3) {
 
   for (int i = 0; i < m; i++) {
@@ -6,5 +7,12 @@ void MultMatriz(int** m1, int** m2, int m, int n, int q, int** m3) {
         m3[i][k] += m1[i][j] * m2[j][k];
       }
     }
+ }
+
+ for(int i = m; i > 0; i--) {
+  for(int j = q; j > 0; j--) {
+    printf("%d ", m3[i][j]);
   }
+  printf("\n");
+ }
 }
