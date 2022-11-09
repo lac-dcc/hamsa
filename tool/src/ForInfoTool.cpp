74fdd42791724b0d0cf79ed5ffbfebcb677bae7e
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2022-11-09 20:05:04 -0300

    Adding early exits to simplify nested if's

--- tool/src/ForInfoTool.cpp
+++ tool/src/ForInfoTool.cpp
@@ -79,84 +79,88 @@ void FindForCondVisitor::traverseExpr(Stmt* node) {
 }
 
 void FindForCondVisitor::handleForInit(Stmt* init, std::string& induc, std::string& valBegin) {
-  if (init) {
-    // Initialization as assignment expression
-    if (auto* assign = dyn_cast<BinaryOperator>(init)) {
-      if (assign->isAssignmentOp()) {
-        if (auto* initVar = dyn_cast<VarDecl>(assign->getLHS()->getReferencedDeclOfCallee()))
-          induc = initVar->getNameAsString();
+  if (!init)
+    return;
 
-        // Initialization with RHS as an integer
-        if (auto* initValInt = dyn_cast<IntegerLiteral>(assign->getRHS())) {
-          valBegin = std::to_string((int)initValInt->getValue().roundToDouble());
-        } else if (auto* initDeclRef = dyn_cast<VarDecl>(assign->getRHS()->getReferencedDeclOfCallee())) {
-          this->inputsBuffer.insert(initDeclRef);
-          valBegin = initDeclRef->getNameAsString();
-        }
+  // Initialization as assignment expression
+  if (auto* assign = dyn_cast<BinaryOperator>(init)) {
+    if (assign->isAssignmentOp()) {
+      if (auto* initVar = dyn_cast<VarDecl>(assign->getLHS()->getReferencedDeclOfCallee()))
+        induc = initVar->getNameAsString();
+
+      // Initialization with RHS as an integer
+      if (auto* initValInt = dyn_cast<IntegerLiteral>(assign->getRHS())) {
+        valBegin = std::to_string((int)initValInt->getValue().roundToDouble());
+      } else if (auto* initDeclRef = dyn_cast<VarDecl>(assign->getRHS()->getReferencedDeclOfCallee())) {
+        this->inputsBuffer.insert(initDeclRef);
+        valBegin = initDeclRef->getNameAsString();
       }
     }
-    // Initialization with a var declaration
-    else if (auto* varDeclStmt = dyn_cast<DeclStmt>(init)) {
-      if (auto* valDecl = dyn_cast<VarDecl>(varDeclStmt->getSingleDecl())) {
-        induc = valDecl->getNameAsString();
-        this->bodyDeclarations.push_back(valDecl);
+  }
+  // Initialization with a var declaration
+  else if (auto* varDeclStmt = dyn_cast<DeclStmt>(init)) {
+    if (auto* valDecl = dyn_cast<VarDecl>(varDeclStmt->getSingleDecl())) {
+      induc = valDecl->getNameAsString();
+      this->bodyDeclarations.push_back(valDecl);
 
-        // Initialization with an integer
-        if (auto* varDeclInt = dyn_cast<IntegerLiteral>(valDecl->getInit()))
-          valBegin = std::to_string((int)varDeclInt->getValue().roundToDouble());
-        // Initialization with another variable
-        else if (auto* varDeclRef =
-                     dyn_cast<VarDecl>(valDecl->getInit()->IgnoreImpCasts()->getReferencedDeclOfCallee())) {
-          this->inputsBuffer.insert(varDeclRef);
-          valBegin = varDeclRef->getNameAsString();
-        } else if (auto* varDeclExpr = dyn_cast<Expr>(valDecl->getInit())) {
-          CharSourceRange srcRange = CharSourceRange::getTokenRange(varDeclExpr->getSourceRange());
-          SourceManager& srcManager = this->Context->getSourceManager();
-          const LangOptions& langOpts = this->Context->getLangOpts();
-          valBegin = Lexer::getSourceText(srcRange, srcManager, langOpts).str();
-          this->traverseExpr(varDeclExpr);
-        }
+      // Initialization with an integer
+      if (auto* varDeclInt = dyn_cast<IntegerLiteral>(valDecl->getInit()))
+        valBegin = std::to_string((int)varDeclInt->getValue().roundToDouble());
+      // Initialization with another variable
+      else if (auto* varDeclRef =
+                   dyn_cast<VarDecl>(valDecl->getInit()->IgnoreImpCasts()->getReferencedDeclOfCallee())) {
+        this->inputsBuffer.insert(varDeclRef);
+        valBegin = varDeclRef->getNameAsString();
+      } else if (auto* varDeclExpr = dyn_cast<Expr>(valDecl->getInit())) {
+        CharSourceRange srcRange = CharSourceRange::getTokenRange(varDeclExpr->getSourceRange());
+        SourceManager& srcManager = this->Context->getSourceManager();
+        const LangOptions& langOpts = this->Context->getLangOpts();
+        valBegin = Lexer::getSourceText(srcRange, srcManager, langOpts).str();
+        this->traverseExpr(varDeclExpr);
       }
     }
   }
 }
 
 void FindForCondVisitor::handleForCond(Expr* cond, std::string& valEnd) {
-  if (cond) {
-    if (auto* bo = dyn_cast<BinaryOperator>(cond)) {
-      auto* boolRHS = bo->getRHS();
-      auto* boolLHS = bo->getLHS();
-      if (auto* condvarL = dyn_cast<VarDecl>(boolLHS->getReferencedDeclOfCallee())) {
-        // For ForCondExpr like "i < n"
-        if (auto* condvarR = dyn_cast<VarDecl>(boolRHS->getReferencedDeclOfCallee())) {
-          this->inputsBuffer.insert(condvarR);
-          valEnd = condvarR->getNameAsString();
-        }
-        // For ForCondExpr like "i > 10"
-        else if (auto* condvalR = dyn_cast<IntegerLiteral>(boolRHS)) {
-          valEnd = std::to_string((int)condvalR->getValue().roundToDouble());
-        }
+  if (!cond)
+    return;
+
+  if (auto* bo = dyn_cast<BinaryOperator>(cond)) {
+    auto* boolRHS = bo->getRHS();
+    auto* boolLHS = bo->getLHS();
+    if (auto* condvarL = dyn_cast<VarDecl>(boolLHS->getReferencedDeclOfCallee())) {
+      // For ForCondExpr like "i < n"
+      if (auto* condvarR = dyn_cast<VarDecl>(boolRHS->getReferencedDeclOfCallee())) {
+        this->inputsBuffer.insert(condvarR);
+        valEnd = condvarR->getNameAsString();
+      }
+      // For ForCondExpr like "i > 10"
+      else if (auto* condvalR = dyn_cast<IntegerLiteral>(boolRHS)) {
+        valEnd = std::to_string((int)condvalR->getValue().roundToDouble());
       }
     }
   }
 }
 
 void FindForCondVisitor::handleForInc(Expr* inc, std::string& increment) {
-  if (inc) {
-    if (auto* unaryOp = dyn_cast<UnaryOperator>(inc)) {
-      if (unaryOp->isIncrementDecrementOp()) {
-        if (unaryOp->isDecrementOp())
-          increment = "-1";
-        else
-          increment = "1";
-      }
+  if (!inc)
+    return;
+
+  if (auto* unaryOp = dyn_cast<UnaryOperator>(inc)) {
+    if (unaryOp->isIncrementDecrementOp()) {
+      if (unaryOp->isDecrementOp())
+        increment = "-1";
+      else
+        increment = "1";
     }
   }
 }
 
 void FindForCondVisitor::handleForBody(Stmt* body, bool nested) {
-  if (body) {
-    if (auto* bodyStmt = dyn_cast<CompoundStmt>(body))
-      this->traverseForBody(bodyStmt, nested);
-  }
+  if (!body)
+    return;
+
+  if (auto* bodyStmt = dyn_cast<CompoundStmt>(body))
+    this->traverseForBody(bodyStmt, nested);
 }
