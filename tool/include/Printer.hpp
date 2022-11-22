14e623d4175a6ecffafc5e5af225a77dbd06240b
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2022-11-22 17:39:39 -0300

    Applying Clang-Format to Printer

--- tool/include/Printer.hpp
+++ tool/include/Printer.hpp
@@ -3,14 +3,14 @@
 #include "LoopInfoTool.hpp"
 
 class Printer {
-  public:
-    virtual void gen_out(DenseMap<int64_t, Kernel*> kernels, ASTContext &Context) = 0;
-    std::string getSourceCodeText(clang::Expr *expr, SourceManager &srcManager, LangOptions langOpts);
+public:
+  virtual void gen_out(DenseMap<int64_t, Kernel*> kernels, ASTContext& Context) = 0;
+  std::string getSourceCodeText(clang::Expr* expr, SourceManager& srcManager, LangOptions langOpts);
 };
 
 class TextPrinter : public Printer {
-  public:
-    virtual void gen_out(DenseMap<int64_t, Kernel*> kernels, ASTContext &Context);
+public:
+  virtual void gen_out(DenseMap<int64_t, Kernel*> kernels, ASTContext& Context);
 };
 
 #endif
