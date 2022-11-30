e50973896c35c7904c2fc01decfc79df68a43e81
1LucasVictor <1victor.lucassilva@gmail.com>
2022-11-30 10:05:29 -0300

    Adjusting run.sh

--- tool/run.sh
+++ tool/run.sh
@@ -2,13 +2,19 @@
 
 LIB="./build/src/libLoopInfoTool.so"
 
-INPUT=$1
-OUTPUT=$2
-FORMAT=$3
+if [ $# -lt 3 ]
+then
+    echo 'Syntax: run input_file output_file format'
+    exit 1
+else
+    INPUT=$1
+    OUTPUT=$2
+    FORMAT=$3
 
-mkdir -p output
+    mkdir -p output
 
-clang++ -cc1 -load $LIB -plugin hamsa \
--plugin-arg-hamsa -output-format -plugin-arg-hamsa $FORMAT \
--plugin-arg-hamsa -output-file -plugin-arg-hamsa $OUTPUT \
-$INPUT
+    clang++ -cc1 -load $LIB -plugin hamsa \
+    -plugin-arg-hamsa -output-format -plugin-arg-hamsa $FORMAT \
+    -plugin-arg-hamsa -output-file -plugin-arg-hamsa $OUTPUT \
+    $INPUT
+fi
