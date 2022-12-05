e366fe8d54e16afba820651c421dd03504efd075
1LucasVictor <1victor.lucassilva@gmail.com>
2022-12-05 10:37:22 -0300

    Fixing Sample 12

--- test/samples/sample12.cpp
+++ test/samples/sample12.cpp
@@ -4,7 +4,7 @@ int main(int agrc, char **argv) {
     int fat, fat2, n, m;
     int sum = 0;
     m = atoi(argv[1]);
-    n = atoi(argv[1]);
+    n = atoi(argv[2]);
     fat = 1; fat2 = 1;
     for (int j = 1; j <= n; j++) {
         for (int i = 1; i <= j; i++) {
