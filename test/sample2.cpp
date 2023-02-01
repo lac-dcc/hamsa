bfdd5180dd933dcc8fe2c2837096f737870621d8
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2023-02-01 15:49:18 -0300

    Adding test samples

---
+++ test/sample2.cpp
@@ -0,0 +1,11 @@
+int main() {
+  if (2) {
+    for (int i = 10; i > 0; i--) {
+      for (int j = 5; j < 10; j++) {
+        for (int j = 10; j <= 10; j++) {
+        }
+      }
+    }
+  }
+  return 0;
+}
---
+++ test/sample5.cpp
@@ -0,0 +1,6 @@
+int main() {
+  int n = 5;
+  for (int i = n; i < 10; i++) {
+  }
+  return 0;
+}
